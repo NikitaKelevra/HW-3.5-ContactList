0347505efa15da9c7fda37a8067eebb43b734faa
Nikita Kelevra <nonamerz5@mail.ru>
2021-06-16 08:57:51 +0300

    Initial Commit

---
+++ HW-3.5-ContactList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW-3.5-ContactList
+//
+//  Created by Сперанский Никита on 16.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
